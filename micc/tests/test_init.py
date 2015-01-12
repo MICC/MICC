28dabb38539a2c6a1c17f19610b51f64525a9fff
Matt Morse <matt@gradf.ly>
2015-01-12 14:36:17 -0500

    tests

--- micc/tests/test_init.py
+++ micc/tests/test_init.py
@@ -1,11 +1,10 @@
 import unittest
-from micc.curves import CurvePair
 import numpy as np
 from sys import stderr
 
 class DistanceTests(unittest.TestCase):
     def test_init(self):
         self.assertTrue(True)
-        
+
 if __name__ == '__main__':
     unittest.main()
